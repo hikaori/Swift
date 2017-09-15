17123b7e745ddedd75fb24f84e61be4a9abc87ea
kaori <kaori55194@gmail.com>
2017-09-15 05:36:59 -0700

    modify

---
+++ Othello/Othello/Othello/Board.swift
@@ -0,0 +1,82 @@
+//
+//  Board.swift
+//  Othello
+//
+//  Created by kaori hirata on 2017-09-12.
+//  Copyright © 2017 kaori hirata. All rights reserved.
+//
+
+import Cocoa
+
+class Board: CustomStringConvertible {
+    
+    let chipDictionaly = [ 0: "🍎", 1: " 🐨", 2: " 🐷", 9: "🌴"]
+    
+    
+    var currentBoard:[[Int]]
+    var rowCount: Int {
+        get {return currentBoard.count - 1}
+    }
+    var colCount: Int{
+        get{return currentBoard[0].count - 1}
+    }
+    var iConZeroCount: Int = 0
+    var iConOneCount : Int = 0
+    var iConTwooCount: Int = 0
+    var iConNineCount: Int = 0
+    
+    
+    init() {
+        self.currentBoard =
+                    [[9,9,9,9,9,9,9,9],
+                     [9,0,0,0,0,0,0,9],
+                     [9,0,0,0,0,0,0,9],
+                     [9,0,0,1,2,0,0,9],
+                     [9,0,0,2,1,0,0,9],
+                     [9,0,0,0,0,0,0,9],
+                     [9,0,0,0,0,0,0,9],
+                     [9,9,9,9,9,9,9,9],
+
+        ]
+    }
+    
+    public var description: String {
+        return "Board: \(currentBoard)"
+    }
+    
+    func displayBoard(board:Board)->Void{
+        var line = 0
+        
+        iConZeroCount = 0
+        iConOneCount  = 0
+        iConTwooCount = 0
+        iConNineCount = 0
+        
+        print("----------------------------------------------------------")
+        for _ in 0...rowCount{
+            for num in self.currentBoard[line] {  // create one row
+                let iconNum = chipDictionaly[num]
+                countIcon(icon:num)
+                print("| \(iconNum!) | ", terminator: "")
+            }
+            print("")
+            print("----------------------------------------------------------")
+            line += 1
+        }
+    }
+    
+    func countIcon(icon:Int){
+        switch icon {
+        case 0:
+            iConZeroCount = iConZeroCount + 1
+        case 1:
+            iConOneCount  = iConOneCount  + 1
+        case 2:
+            iConTwooCount = iConTwooCount + 1
+        case 9:
+            iConNineCount = iConNineCount + 1
+        default:
+            iConZeroCount = iConZeroCount + 0
+        }
+    }
+}
